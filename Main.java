8bdedbf469b52263230b11f4234d39c4a8611b84
AtharvAwle <atharvawle0156@gmail.com>
2025-04-13 20:40:18 +0530

    wrote logic for scrambling words from wordList

--- Main.java
+++ Main.java
@@ -1,5 +1,50 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rc = new Random();
 
+        String[] wordlist = {"Atharv","Ayush","Srushti","Shlok","Atul","Arya","Kailash","Fluffy","Dudu","Chapru"};
+
+        int playerScore = 0;
+        Boolean stillPlaying = true;
+        System.out.println("Welcome to my Word Scrambler Wizard!");
+        System.out.println("You have to unscrable the words to score the point");
+
+        while (stillPlaying){
+            String word = wordlist[rc.nextInt(wordlist.length)];
+            String scrambledWord = scrambleWord(word,rc);
+            System.out.println("Guess the correct word: "+ scrambledWord);
+            boolean wordGuessed = false;
+            int attempts = 3;
+
+            while (attempts > 0 && !wordGuessed){
+                System.out.println("Your Guess: ");
+                String playerGuess = sc.nextLine();
+
+                if (playerGuess.equalsIgnoreCase(word)){
+                    wordGuessed = true;
+                    System.out.println("You Guessed Right");
+                    playerScore++;
+                    System.out.println("Your Score: "+playerScore);
+                }else {
+                    System.out.println("Incorrect Guess");
+                    attempts--;
+                    System.out.println("Attempts Remaining: "+attempts);
+                }
+            }
+        }
+
+    }
+
+    public static String scrambleWord(String word , Random rc){
+        char[] letters = word.toCharArray();
+        for(int i = 0 ; i < letters.length ; i++){
+            int j = rc.nextInt(letters.length);
+            char temp = letters[i];
+            letters[i] = letters[j];
+            letters[j] = temp;
+        }
+        return new String(letters);
     }
 }
